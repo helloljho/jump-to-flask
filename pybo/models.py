871964f4bb7cc2cb3a436864116fec75555a1a9f
helloljho <helloljho@gmail.com>
2021-12-10 13:31:27 +0900

    fix: question simple list

--- pybo/models.py
+++ pybo/models.py
@@ -5,7 +5,8 @@ class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
-    create_time = db.Column(db.DateTime(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -7,6 +9,14 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def hello_pybo():
     return 'hello, pybo~~~!!'
 
+
 @bp.route('/')
 def index():
-    return 'pybo index'
+    question_list = Question.query.order_by(Question.create_date.desc())
+    return render_template('question/question_list.html', question_list=question_list)
+
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get(question_id)
+    return render_template('question/question_detail.html', question=question)
